de25130430aca14dff4941f81ac823620d23fb59
Cassidy Johnson <cassidyrose56@gmail.com>
2023-01-05 11:08:32 -0600

    formatting on techs

--- client/src/components/PersonalInfo.js
+++ client/src/components/PersonalInfo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../styles/personal-info.scss';
+import { motion } from 'framer-motion';
 
 const html = require('../../../assets/html.png');
 const css = require('../../../assets/css.png');
 const sass = require('../../../assets/sass.png');
 const react = require('../../../assets/react.png');
 const reactRouter = require('../../../assets/react-router.png');
-const framerMotion = require('../../../assets/framer.png');
 const mongoDB = require('../../../assets/mongodb.svg');
 const postgres = require('../../../assets/postgres.png');
 const graphQL = require('../../../assets/GraphQL.png');
@@ -35,11 +35,27 @@ const PersonalInfo = () => {
                         on client-side applications... 
                     </h3>
                     <section id='frontend-icons' className='icon-sections'>
-                        <img src={html} id='html' className='icon' title='HTML' alt='HTML'></img>
-                        <img src={css} id='css' className='icon' title='CSS' alt='CSS'></img>
-                        <img src={sass} id='sass' className='icon' title='Sass' alt='Sass'></img>
-                        <img src={react} id='react' className='icon' title='React' alt='React'></img>
-                        <img src={reactRouter} id='react-router' className='icon' title='React Router' alt='React Router'></img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }} 
+                            whileInView={{ opacity: 1 }} 
+                            src={html} id='html' className='icon' title='HTML' alt='HTML'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }} 
+                            whileInView={{ opacity: 1 }}  src={css} id='css' className='icon' title='CSS' alt='CSS'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }} 
+                            whileInView={{ opacity: 1 }}  src={sass} id='sass' className='icon' title='Sass' alt='Sass'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }} 
+                            whileInView={{ opacity: 1 }}  src={react} id='react' className='icon' title='React' alt='React'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }} 
+                            whileInView={{ opacity: 1 }}  src={reactRouter} id='react-router' className='icon' title='React Router' alt='React Router'></motion.img>
                     </section>
                 </section>
                 <section id='backend' className='tech-sections'>
@@ -47,11 +63,21 @@ const PersonalInfo = () => {
                         backend applications...
                     </h3>
                     <section id='backend-icons' className='icon-sections'>
-                        <img src={node} id='node' className='icon' title='Node.js' alt='node.js'></img>
-                        <img src={express} id='express' className='icon' title='Express' alt='express'></img>
-                        <img src={mongoDB} id='MongoDB' className='icon' title='MongoDB' alt='mongodb'></img>
-                        <img src={postgres} id='PostgreSQL' className='icon' title='PostgreSQL' alt='PostgreSQL'></img>
-                        <img src={graphQL} id='graphQL' className='icon' title='GraphQL' alt='GraphQL'></img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={node} id='node' className='icon' title='Node.js' alt='node.js'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={express} id='express' className='icon' title='Express' alt='express'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={mongoDB} id='MongoDB' className='icon' title='MongoDB' alt='mongodb'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={postgres} id='PostgreSQL' className='icon' title='PostgreSQL' alt='PostgreSQL'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={graphQL} id='graphQL' className='icon' title='GraphQL' alt='GraphQL'></motion.img>
                     </section>
                 </section>
                 <section id='more' className='tech-sections'>
@@ -59,11 +85,21 @@ const PersonalInfo = () => {
                     ... and more!
                     </h3>
                     <section id='more-icons' className='icon-sections'>
-                        <img src={git} id='git' className='icon' title='Git' alt='Git'></img>
-                        <img src={github} id='github' className='icon' title='Github' alt='Github'></img>
-                        <img src={webpack} id='webpack' className='icon' title='Webpack' alt='Webpack'></img>
-                        <img src={aws}id='aws' className='icon' title='AWS' alt='AWS'></img>
-                        <img src={typescript} id='typescript' className='icon' title='Typescript' alt='Typescript'></img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={git} id='git' className='icon' title='Git' alt='Git'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={github} id='github' className='icon' title='Github' alt='Github'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={webpack} id='webpack' className='icon' title='Webpack' alt='Webpack'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }} src={aws}id='aws' className='icon' title='AWS' alt='AWS'></motion.img>
+                        <motion.img
+                            whileHover={{ scale: 1.1 }}
+                            whileInView={{ opacity: 1 }}mg src={typescript} id='typescript' className='icon' title='Typescript' alt='Typescript'></motion.img>
                     </section>
                 </section>
                 
