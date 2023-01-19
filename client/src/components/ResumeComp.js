11e44b8df140cf67272fafcd2d907b3ae7dd489d
Cassidy Johnson <cassidyrose56@gmail.com>
2023-01-19 16:45:33 -0600

    added navmenu media queries and changes some padding on major sections

--- client/src/components/ResumeComp.js
+++ client/src/components/ResumeComp.js
@@ -13,7 +13,7 @@ const ResumeComp = (props) => {
         <div id='resume-comp-layout'>
             <section id='title-and-pic'>
                 <motion.a 
-                    whileHover={{ scale: 1.1 }}
+                    whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.9 }}
                     transition={{ type: "spring", stiffness: 400, damping: 17 }} 
                     id='img-box' 
